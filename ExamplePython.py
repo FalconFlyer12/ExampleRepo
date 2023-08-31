2db802114733a079a4a76b8a816bb63da96a503d
FalconFlyer12 <143207992+FalconFlyer12@users.noreply.github.com>
2023-08-31 09:13:11 -0700

    Update ExamplePython.py
    8/31/23 11:13: Added a Jobs List

--- ExamplePython.py
+++ ExamplePython.py
@@ -8,3 +8,7 @@ print (f"x = {x} and y = {y}")
 
 print ("c = {c} and d = {d}".format(c=1, d=2))
 
+job_list = {'James F Mack': 'REDACTED', "Mary A North": 'Commander_A21', 'Timothy I C King': 'Clock_Repairman_E67'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
